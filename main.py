f1a409f3e2cc5c58d92321ab9b0d3a08ea566a7e
Afif Akromi <massachusetts48@gmail.com>
2022-03-05 19:48:36 +0700

    add generate_layer

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import numpy as np
 import math
 
+"""Global Variables"""
+n_neurons = 0
+weights = []
+biases = []
+act_function = {}
+act_value = {}
+
+
 """RELU Activation Function"""
 def relu(x):
   return float(max(x,0))
@@ -11,8 +19,29 @@ def linear(x):
 
 """Sigmoid Activation Function"""
 def sigmoid(x):
-  return 1/(1+math.exp(-x))
+  return float(1/(1 + math.exp(-x)))
 
 """Softmax Activation Function"""
 def softmax(x):
-  return np.exp(x) / np.sum(np.exp(x))
+  return float(np.exp(x) / np.sum(np.exp(x)))
+
+
+def generate_layer(N_NEURONS, ACT_FUNCTION, WEIGHTS, BIASES):
+  activations_func = {
+    'linear': linear,
+    'sigmoid': sigmoid,
+    'softmax': softmax,
+    'relu': relu
+  }
+
+  if(N_NEURONS < 1):
+    raise ValueError("Neuron harus lebih dari 0")
+
+  if(activations_func in ('linear', 'sigmoid', 'relu', 'softmax')):
+    n_neurons = N_NEURONS
+    act_function = activations_func[ACT_FUNCTION]
+    weights = WEIGHTS
+    biases = BIASES
+    activation_value = None
+  else:
+    raise ValueError("Aktifasi fungsi harus salah satu dari 'linear', 'sigmoid', 'relu', 'softmax'")
